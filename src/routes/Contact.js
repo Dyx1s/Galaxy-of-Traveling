0db91b86a71d80644192463e543956079fe4bc8a
Dyx1s <grafdonskiy@gmail.com>
2024-04-07 19:26:14 +0300

    finish the project, add new favicon

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroImage from '../components/HeroImage'
+import Footer from '../components/Footer'
+import Form from '../components/Form'
 
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
-      <HeroImage/>
+      <HeroImage heading='Contact.' text='Get in touch.'/>
+      <Form/>
+      <Footer/>
     </div>
   )
 }
